3841711fe8ad5368cdd4e96fd05607ec035adf7a
Elvis Magagula <kopahead@gmail.com>
2015-03-02 04:20:28 +0200

    partials footer add
    Signed-off-by: Elvis Magagula <kopahead@gmail.com>

---
+++ app/views/partials/footer_editor.blade.php
@@ -0,0 +1,8 @@
+{{HTML::script("packages/ckeditor/plugins/styles/styles/default.js")}}
+{{HTML::script("packages/ckeditor/plugins/pastefromword/filter/default.js")}}
+{{HTML::script("packages/ckeditor/plugins/templates/templates/default.js")}}
+{{HTML::script("packages/ckeditor/config.js")}}
+{{HTML::script("packages/ckeditor/ckeditor.js")}}
+<script>
+    CKEDITOR.replace( 'editor_area' );
+</script>
