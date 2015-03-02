c70e9aa6bdf7ceabe16f4396056c9322d47ac4c4
Elvis Magagula <kopahead@gmail.com>
2015-03-02 11:55:26 +0200

    Signed-off-by: Elvis Magagula <kopahead@gmail.com>

--- app/views/partials/post/post_details.blade.php
+++ app/views/partials/post/post_details.blade.php
@@ -1,2 +1,5 @@
-{{display_name_of($post->user->username) == "" ? $post->user->username : display_name_of($post->user->username)}}<br>
-{{$post->created_at->diffForHumans()}}<br>
+<blockquote>
+  <p>Posted By: {{display_name_of($post->user->username) == "" ? $post->user->username : display_name_of($post->user->username)}}</p>
+  <footer>About <cite title="Source Title">{{$post->created_at->diffForHumans()}}</cite></footer>
+</blockquote>
+<hr>
