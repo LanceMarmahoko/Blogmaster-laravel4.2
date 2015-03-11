ba2b6f64a921cb3fd00bb8e1ba8170e3e5af7a0b
Elvis Magagula <kopahead@gmail.com>
2015-03-11 12:38:27 +0200

    Changed json output

--- app/controllers/PagesController.php
+++ app/controllers/PagesController.php
@@ -11,12 +11,12 @@ class PagesController extends \BaseController {
     public function index(){
         //homepage prep, display published posts
         $posts = Post::wherePublish_status(true)->orderBy('id', 'DESC')->get();
-        return View::make('posts.show',compact('posts'));
+        return Response::json($posts)->setCallback(Input::get('callback'));
     }
 
     public function show($id){
         $post = Post::with('user')->wherePublish_status(true)->findOrFail($id);
-        return Response::json($post)->setCallback(Input::get('callback'));
+        return View::make('posts.show',compact('posts'));
 }
     public function dashboard(){
         $published = Post::wherePublish_status(true)->orderBy('id', 'DESC')->paginate(10);  //pagination
